da520a8bc8b8a292ee9643003701685b3a85eada
Juan Cruz Riera <juancriera94@gmail.com>
2023-11-13 19:18:32 -0300

    Added a test for the Lion

--- zoo-app/tests/Lion.test.js
+++ zoo-app/tests/Lion.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import { Lion } from '../src/model/Lion.js'
-import { assert } from 'chai'
+import { assert, expect } from 'chai'
 
 describe('Lion', () => {
   it('should speak with a roar', () => {
@@ -8,5 +8,11 @@ describe('Lion', () => {
     const result = lion.speak('I\'m a lion')
     console.log(result)
     assert.strictEqual(result, 'I\'m roar a roar lion roar')
+  })
+
+  it('should throw an exception when speaking with a null string', () => {
+    const lion = new Lion()
+    // Use Chai's expect().to.throw() to check if an exception is thrown
+    expect(() => lion.speak(null)).to.throw('Invalid input. Please provide a valid input as a string.')
   })
 })
